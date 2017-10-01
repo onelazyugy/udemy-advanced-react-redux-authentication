cadb961c538cb3c0136ebca4c1dab3457a83f358
viet <viettangle12@gmail.com>
2017-10-01 17:58:08 -0400

    add signout feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { browserHistory } from "react-router";
-import { AUTH_USER, AUTH_ERROR } from "./types";
+import { AUTH_USER, AUTH_ERROR , UNAUTH_USER} from "./types";
 
 const ROOT_URL = "http://localhost:3090";
 
@@ -36,4 +36,9 @@ export function authError(error) {
         type: AUTH_ERROR,
         payload: error
     }
+}
+
+export function signoutUser() {
+    localStorage.removeItem('token');
+    return {type: UNAUTH_USER}
 }
