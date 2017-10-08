bc81d3d5f404fb89fb4d70ac877be5470fd2c2a5
viet <viettangle12@gmail.com>
2017-10-08 13:11:47 -0400

    wrap up input form validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -13,33 +13,51 @@ class Signup extends Component {
       fields: { email, password, passwordConfirm }
     } = this.props;
     return (
-        <form> 
-            <fieldset className="form-group">
-                <label>Email:</label>
-                <input className="form-control" {...email}/>
-            </fieldset>
-            <fieldset className="form-group">
-                <label>Password:</label>
-                <input className="form-control" type="password" {...password}/>
-                {password.touched && password.error && <div className="error">{password.error}</div>}
-            </fieldset>
-            <fieldset className="form-group">
-                <label>Confirm Password:</label>
-                <input className="form-control" type="password" {...passwordConfirm}/>
-            </fieldset>
-            <button action="submit" className="btn btn-primary">Sign up!</button>
-        </form>
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" {...email} />
+          {email.touched &&
+          email.error && <div className="error">{email.error}</div>}
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input className="form-control" type="password" {...password} />
+          {password.touched &&
+          password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input className="form-control" type="password" {...passwordConfirm} />
+          {passwordConfirm.touched &&
+          passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
+        </fieldset>
+        <button action="submit" className="btn btn-primary">
+          Sign up!
+        </button>
+      </form>
     );
   }
 }
 
 function validate(formProps) {
-    const errors = {};
-    console.log(formProps);
-    if (formProps.password !== formProps.passwordConfirm) {
-        errors.password = 'Passwrod must match';
-    }
-    return errors;
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = "Please enter an email";
+  }
+  if (!formProps.password) {
+    errors.password = "Please enter an password";
+  }
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter an password confirmation";
+  }
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = "Passwrod must match";
+  }
+  return errors;
 }
 
 export default reduxForm({
