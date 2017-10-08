a71d8b7a1dbdcb5f6e31f525d4480b29d217d4b4
viet <viettangle12@gmail.com>
2017-10-08 13:58:45 -0400

    wrap up signup feature

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -7,13 +7,28 @@ class Signup extends Component {
     super(props);
   }
 
+  handleFormSubmit(formProps) {
+    // call action creator to sign up the user!
+    this.props.signupUser(formProps);
+  }
+
+  renderAlert() {
+      if (this.props.errorMessage) {
+        return (
+        <div className="alert alert-danger">
+            <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+        );
+      }
+  }
+
   render() {
     const {
       handleSubmit,
       fields: { email, password, passwordConfirm }
     } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
@@ -34,6 +49,7 @@ class Signup extends Component {
           {passwordConfirm.touched &&
           passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">
           Sign up!
         </button>
@@ -60,8 +76,12 @@ function validate(formProps) {
   return errors;
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: "signup",
   fields: ["email", "password", "passwordConfirm"],
   validate
-})(Signup);
+}, mapStateToProps, actions)(Signup);
