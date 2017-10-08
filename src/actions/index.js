b5c064b64d4ec815d47e8a4f57dfa343e09e82f9
viet <viettangle12@gmail.com>
2017-10-08 14:52:24 -0400

    finish

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { browserHistory } from "react-router";
-import { AUTH_USER, AUTH_ERROR , UNAUTH_USER} from "./types";
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from "./types";
 
 const ROOT_URL = "http://localhost:3090";
 
@@ -12,10 +12,9 @@ export function signinUser({ email, password }) {
     axios
       .post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
-
         //if request is good...
         //-update state to indicate user is authenticated
-        dispatch({type: AUTH_USER});
+        dispatch({ type: AUTH_USER });
 
         //-save the JWT token
         localStorage.setItem("token", response.data.token);
@@ -26,36 +25,53 @@ export function signinUser({ email, password }) {
       .catch(() => {
         //if request is bad...
         //-show an error to the user
-        dispatch(authError('Bad Login Info'));
+        dispatch(authError("Bad Login Info"));
       });
   };
 }
 
-export function signupUser({email, password}) {
+export function signupUser({ email, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/signup`, {email, password})
+    axios
+      .post(`${ROOT_URL}/signup`, { email, password })
       .then(response => {
-        dispatch({type: AUTH_USER});
-        localStorage.setItem('token', response.data.token);
-        browserHistory.push('/feature');
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem("token", response.data.token);
+        browserHistory.push("/feature");
       })
       .catch(response => dispatch(authError(response.response.data.error)));
-      // .catch(response => {
-      //   debugger;
-      //   const errorMsg = response.data.error;
-      //   dispatch(authError(response.data.error));
-      // });
-  }
+    // .catch(response => {
+    //   debugger;
+    //   const errorMsg = response.data.error;
+    //   dispatch(authError(response.data.error));
+    // });
+  };
 }
 
 export function authError(error) {
-    return {
-        type: AUTH_ERROR,
-        payload: error
-    }
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
 
 export function signoutUser() {
-    localStorage.removeItem('token');
-    return {type: UNAUTH_USER}
+  localStorage.removeItem("token");
+  return { type: UNAUTH_USER };
 }
+
+export function fetchMessage() {
+  //using redux thunk here, return a function instead of an object
+  return function(dispatch) {
+    axios
+      .get(ROOT_URL, {
+        headers: { authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        dispatch({type: FETCH_MESSAGE, payload: response.data.message})
+      });
+  };
+}
+
+
+//use redux promise instead of redux thunk
--- src/components/feature.js
+++ src/components/feature.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 
 class Feature extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
   render() {
-    return <div>Feature</div>;
+    return <div>{this.props.message}</div>;
   }
 }
 
-export default Feature;
+function mapStateToProps(state) {
+  return {
+    message: state.auth.message
+  }
+}
+
+export default connect(mapStateToProps, actions)(Feature);
